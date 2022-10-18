450d96e64d311f9f5cbb82d0ae055a1bfb71627a
Susibakao2o2oo3 <116031780+Susibakao2o2oo3@users.noreply.github.com>
2022-10-18 21:08:27 +0200

    Educational pourpouse only
    Just a password cracker.

--- Password cracker.py
+++ Password cracker.py
@@ -1,18 +1,16 @@
 from random import *
-gueses = 0
-print("use only:abcdefghijklnoprstuwxyzvq")
 user_pass = input("Enter your password:")
 password = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 
             'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't','u','v', 
             'w', 'x', 'y', 'z',]
 guess = ""
+countofgueses = 0
 while (guess != user_pass):
-    gueses += 1
     guess = ""
+    countofgueses += 1
     for letter in range(len(user_pass)):
         guess_letter = password[randint(0, 25)]
         guess = str(guess_letter) + str(guess)
     print(guess)
-print("Your password is ",guess)
-
-print("Guesed " + str(gueses) + " times")
+print("Your password is",guess)
+print("Guesed " + str(countofgueses) + " times.")
